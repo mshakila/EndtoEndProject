780c53d52c4b8cbae523880d77161f7c960f90a1
mns.she <mns.she@gmail.com>
2024-06-04 07:29:34 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 
 from setuptools import find_packages , setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(filepath:str)->List(str):
+    requirements = []
+    with open(file_path) as file_obj:   # opens requirements file
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements] # keeps all requirements inside list
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+        
+    return requirements
 
 setup(
     name = "DiamondPricePrediction",
     version = "0.0.1", 
-    author = "M Shakila",
-    author_email = "mns.she@gmail.com",
+    author = "binte Sajida Khanum",
+    author_email = "xxxxxx@gmail.com",
+    #install_requires = ["scikit-learn","pandas", "numpy"] # here doing manually
+    install_requirements = get_requirements("requirements.txt") # here, we are getting requirements , this is automated
     packages = find_packages()
 )
